5f515cb021b7d489849ea832b1b4c2f58e54b662
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-08-21 23:43:18 -0700

    init

--- output.tf
+++ output.tf
@@ -62,3 +62,18 @@ output "private_ips" {
 output "public_ips" {
   value = ["${module.dcos-pvtagt-instances.public_ips}"]
 }
+
+# Tested DCOS OSes Name
+output "dcos_instance_os" {
+  value = "${var.dcos_instance_os}"
+}
+
+# Private Node Self Link
+output "instances_self_link" {
+  value = ["${module.dcos-pvtagt-instances.instances_self_link}"]
+}
+
+# Private Node Target Pool Self link
+output "target_pool" {
+  value = ["${module.dcos-pvtagt-instances.target_pool}"]
+}
