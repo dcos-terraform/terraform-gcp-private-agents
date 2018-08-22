9c784b513cf3b1602782da7e23e0fcb187084324
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-08-22 01:39:34 -0700

    init

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
 provider "google" {}
 
-module "dcos-tested-oses" {
-  source  = "dcos-terraform/gcp-tested-oses/template"
-  version = "~> 0.0"
-
-  providers = {
-    google = "google"
-  }
-
-  os = "${var.dcos_instance_os}"
-}
-
 module "dcos-pvtagt-instances" {
   #  source  = "dcos-terraform/instance/gcp"  #  version = "~> 0.0"
 
@@ -23,8 +12,8 @@ module "dcos-pvtagt-instances" {
   name_prefix              = "${var.name_prefix}"
   hostname_format          = "${var.hostname_format}"
   num_instances            = "${var.num_private_agents}"
-  image                    = "${coalesce(var.image, module.dcos-tested-oses.gcp_image_name)}"
-  user_data                = "${var.image == "" ? module.dcos-tested-oses.os-setup : var.user_data}"
+  image                    = "${var.image}"
+  user_data                = "${var.user_data}"
   machine_type             = "${var.machine_type}"
   instance_subnetwork_name = "${var.private_agent_subnetwork_name}"
   ssh_user                 = "${var.ssh_user}"
@@ -33,4 +22,6 @@ module "dcos-pvtagt-instances" {
   disk_type                = "${var.disk_type}"
   disk_size                = "${var.disk_size}"
   tags                     = "${var.tags}"
+  dcos_instance_os         = "${var.dcos_instance_os}"
+  dcos_version             = "${var.dcos_version}"
 }
