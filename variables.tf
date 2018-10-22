325879d43defb7ab51ed91d7b46a578726bbcc02
Weston Bassler <wbassler@mesosphere.com>
2018-10-22 16:15:16 -0400

    update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "num_private_agents" {
 
 # Cluster Name
 variable "cluster_name" {
-  description = "Cluster Name"
+  description = "Name of the DC/OS cluster"
 }
 
 # Instance Type
 variable "machine_type" {
-  description = "machine type"
+  description = "Instance Type"
 }
 
 # DCOS version
@@ -20,29 +20,29 @@ variable "dcos_version" {
 
 # Element by zone list
 variable "zone_list" {
-  description = "zone list"
+  description = "Element by zone list"
   type        = "list"
   default     = []
 }
 
 # Source image to boot from
 variable "image" {
-  description = "image"
+  description = "Source image to boot from"
 }
 
 # Disk Type to Leverage
 variable "disk_type" {
-  description = "Disk Type to Leverage. The GCE disk type. Can be either 'pd-ssd', 'local-ssd', or 'pd-standard'. (optional)"
+  description = "Disk Type to Leverage"
 }
 
 # Disk Size in GB
 variable "disk_size" {
-  description = "disk size"
+  description = "Disk Size in GB"
 }
 
 # Instance Subnetwork Name
 variable "private_agent_subnetwork_name" {
-  description = "private agent subnetwork name"
+  description = "Instance Subnetwork Name"
 }
 
 # Customer Provided Userdata
@@ -53,12 +53,12 @@ variable "user_data" {
 
 # SSH User
 variable "ssh_user" {
-  description = "ssh user"
+  description = "SSH User"
 }
 
 # SSH Public Key
 variable "public_ssh_key" {
-  description = "public ssh key"
+  description = "SSH Public Key"
 }
 
 # Add special tags to the resources created by this module
@@ -82,7 +82,7 @@ variable "dcos_instance_os" {
 
 # Deploy instance with preemptible scheduling. (bool)
 variable "scheduling_preemptible" {
-  description = "scheduling preemptible"
+  description = "Deploy instance with preemptible scheduling. (bool)"
   default     = "false"
 }
 
